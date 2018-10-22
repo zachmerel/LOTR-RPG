bd73180de1c01718d8962b45cff3c5aed9cd98a9
Zach Merel <zachmerel@gmail.com>
2018-10-22 17:55:39 -0500

    test

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Set Character HP
+var gandalfHP = 180;
+var legolasHP = 150;
+var gimliHP = 120;
+var gollumHP = 100;
+
+//push HP to HTML
+$('#gandalfHP').append(gandalfHP);
+$('#gimliHP').append(gimliHP);
+$('#legolasHP').append(legolasHP);
+$('#gollumHP').append(gollumHP);
+
+$("#gandalfpic").on("click", function(){
+    var gandalfCT = $("<div>");
+    gandalfCT.setAttribute("src", "gandalf.jpg");
+    gandalfCT.addClass("character-tile character-background") 
+    gandalfHP.addID
+    $('#gandalfCT').append(your-character);
+});
